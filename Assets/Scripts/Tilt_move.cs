69030f4094c74866ddb4e516157dbb1afcb55537
Ingrid <ingridmh99@hotmail.com>
2020-05-09 20:02:15 +0200

    Commited changes

---
+++ Assets/Scripts/Tilt_move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Tilt_move : MonoBehaviour
+    {
+        float speed = 10F;
+        Rigidbody2D rb2d;
+
+        void Start()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+        }
+
+        void FixedUpdate()
+        {
+            Vector3 acc = Input.acceleration;
+            rb2d.AddForceAtPosition(acc.x * speed, 0, acc.y * speed);
+        }
+    }
+
